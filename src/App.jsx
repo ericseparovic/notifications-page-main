387435402b646f01ad916b82f058a19e5ef7abc6
Eric Separovic <ericseparovic07@gmail.com>
2022-12-27 09:48:54 -0300

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container mx-auto font-jakartaSans px-4">
+      <header className="flex justify-between items-center h-20">
+        <h2 className="text-xl font-bold">
+          Notifications{" "}
+          <span className="bg-Blue px-2 py-0 ml-3 rounded-lg text-white">
+            3
+          </span>
+        </h2>
+
+        <p className="text-DarkGrayishBlue">Mark all as read</p>
+      </header>
+      <main>
+        <article>
+          <div>
+            <img src="src/assets/images/avatar-mark-webber.webp" alt="avatar" />
+          </div>
+          <div>
+            <p>
+              <span>Mark Webber</span>
+              reacted to your recent post My first tournament today!
+            </p>
+            <p>1m ago</p>
+          </div>
+        </article>
+      </main>
+    </div>
+  );
 }
 
 export default App;
